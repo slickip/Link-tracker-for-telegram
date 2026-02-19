9ad98a7c7878c3388de0121d401decd3670f9381
hehe <irinapoltorakova404@gmail.com>
2026-02-19 17:57:53 +0300

    Make program compilable - Write main - Fix config.go (add new library) - Add SetCommand (registrate commands in telegram keyboard)

--- internal/application/commands/help.go
+++ internal/application/commands/help.go
@@ -1,10 +1,14 @@
-package command
+package commands
 
 import "gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/domain"
 
 type HelpCommand struct{}
 
 var _ domain.Command = (*HelpCommand)(nil)
+
+func NewHelpCommand() domain.Command {
+	return &HelpCommand{}
+}
 
 func (c *HelpCommand) Name() string {
 	return "/help"
