f707a1989e0112ca095a24dd4899e043db885143
hehe <irinapoltorakova404@gmail.com>
2026-02-19 15:26:30 +0300

    Add command - Add Command interface in domain - Add /start command - Add /help command - Add unknown command

---
+++ internal/application/help.go
@@ -0,0 +1,15 @@
+package application
+
+import "gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/domain"
+
+type HelpCommand struct{}
+
+var _ domain.Command = (*HelpCommand)(nil)
+
+func (c *HelpCommand) Name() string {
+	return "/help"
+}
+
+func (c *HelpCommand) Execute(chatID int64) (string, error) {
+	return "Пока ничего :(", nil
+}
---
+++ internal/application/unknown.go
@@ -0,0 +1,15 @@
+package application
+
+import "gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/domain"
+
+type UnknownCommand struct{}
+
+var _ domain.Command = (*UnknownCommand)(nil)
+
+func (c *UnknownCommand) Name() string {
+	return "unknown"
+}
+
+func (c *UnknownCommand) Execute(chatID int64) (string, error) {
+	return "Неизвестная команда. Воспользуйтесь /help, чтобы посмотреть доступные команды", nil
+}
