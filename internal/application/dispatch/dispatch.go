a4b83930b543551d9670d9fc35bd2e464983cc5d
hehe <irinapoltorakova404@gmail.com>
2026-03-04 11:59:41 +0300

    refactor: treat unknown command as regular command

--- internal/application/dispatch/dispatch.go
+++ internal/application/dispatch/dispatch.go
@@ -8,10 +8,9 @@ import (
 
 type Dispatcher struct {
 	commands map[string]domain.Command
-	unknown  domain.Command
 }
 
-func NewDispatcher(commands []domain.Command, unknown domain.Command) *Dispatcher {
+func NewDispatcher(commands []domain.Command) *Dispatcher {
 	cmdMap := make(map[string]domain.Command)
 
 	for _, cmd := range commands {
@@ -20,20 +19,19 @@ func NewDispatcher(commands []domain.Command, unknown domain.Command) *Dispatche
 
 	return &Dispatcher{
 		commands: cmdMap,
-		unknown:  unknown,
 	}
 }
 
 func (d *Dispatcher) Dispatch(text string) domain.Command {
 	if text == "" {
-		return d.unknown
+		return d.commands["unknown"]
 	}
 
 	commandName := strings.Split(text, " ")[0]
 
 	cmd, ok := d.commands[commandName]
 	if !ok {
-		return d.unknown
+		return d.commands["unknown"]
 	}
 
 	return cmd
