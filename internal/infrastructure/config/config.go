be959c5c44dbb089df0309a08cffdc440df7aa55
hehe <irinapoltorakova404@gmail.com>
2026-03-08 18:51:33 +0300

    Add registration with /start command

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -9,20 +9,27 @@ import (
 
 type Config struct {
 	TelegramToken string
+	ScrapperURL   string
 }
 
 func MustLoad() *Config {
+
 	if err := godotenv.Load(); err != nil {
 		log.Println("no .env file found")
 	}
 
 	token := os.Getenv("TELEGRAM_TOKEN")
-
 	if token == "" {
 		log.Fatal("TELEGRAM_TOKEN is not set")
 	}
 
+	scrapperURL := os.Getenv("SCRAPPER_URL")
+	if scrapperURL == "" {
+		log.Fatal("SCRAPPER_URL is not set")
+	}
+
 	return &Config{
 		TelegramToken: token,
+		ScrapperURL:   scrapperURL,
 	}
 }
