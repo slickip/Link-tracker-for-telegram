5cb2a1e5d71fdd0a823091148cf8e713b127014a
hehe <irinapoltorakova404@gmail.com>
2026-03-08 16:47:31 +0300

    Create new domain entities - Create chat.go (keeps user id) - Create link.go (describes tracked resourses, url parsing, LinkSourse enum) - Create update.go (defines LinkUpdate entity used for communication between) - Create track.go (defines state machine structures used during the /track dialogue) - Add new errors in pkg/errors

---
+++ internal/domain/track.go
@@ -0,0 +1,18 @@
+package domain
+
+type TrackState string
+
+const (
+	StateIdle TrackState = "idle"
+
+	StateWaitingForURL  TrackState = "waiting_for_url"
+	StateWaitingForTags TrackState = "waiting_for_tags"
+)
+
+type TrackSession struct {
+	ChatID int64
+
+	State TrackState
+
+	URL string
+}
