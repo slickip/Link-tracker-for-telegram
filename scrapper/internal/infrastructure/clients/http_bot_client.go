131024dc7c2e298582749e6a46019fcc01c1a8dc
hehe <irinapoltorakova404@gmail.com>
2026-03-17 04:13:15 +0300

    fix for linter

--- scrapper/internal/infrastructure/clients/http_bot_client.go
+++ scrapper/internal/infrastructure/clients/http_bot_client.go
@@ -51,7 +51,9 @@ func (c *HTTPBotClient) SendUpdate(ctx context.Context, update domain.LinkUpdate
 		return err
 	}
 
-	defer resp.Body.Close()
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("bot returned status %d", resp.StatusCode)
