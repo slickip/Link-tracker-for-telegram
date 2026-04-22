c4546c9cc391f1f869800dd2b5660fe938a4c507
hehe <irinapoltorakova404@gmail.com>
2026-04-22 15:29:03 +0300

    Resolve merge conflicts with dev/main 2

--- scrapper/internal/config/config.go
+++ scrapper/internal/config/config.go
@@ -1,140 +1,74 @@
-package services
+package config
 
 import (
-	"context"
-	"time"
+	"log"
+	"os"
 
-	"github.com/go-co-op/gocron/v2"
-
-	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/pkg/logger"
-	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/scrapper/internal/domain"
-	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/scrapper/internal/domain/repositories"
-	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/scrapper/internal/infrastructure/clients"
-	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/scrapper/internal/infrastructure/parsers"
+	"github.com/joho/godotenv"
 )
 
-const seconds = 30
-
-type Scheduler struct {
-	repo repositories.TrackingRepository
-
-	githubClient *clients.GitHubClient
-	soClient     *clients.StackOverflowClient
-	botClient    clients.BotClient
-	log          *logger.Slog
+type Config struct {
+	BotHTTPURL       string
+	BotGRPCAddr      string
+	ScrapperHTTPAddr string
+	ScrapperGRPCAddr string
+	DatabaseURL      string
+	AccessType       AccessType
 }
 
-func NewScheduler(
-	repo repositories.TrackingRepository,
-	githubClient *clients.GitHubClient,
-	soClient *clients.StackOverflowClient,
-	botClient clients.BotClient,
-	log *logger.Slog,
-) *Scheduler {
-	return &Scheduler{
-		repo:         repo,
-		githubClient: githubClient,
-		soClient:     soClient,
-		botClient:    botClient,
-		log:          log,
-	}
-}
+type AccessType string
 
-func (s *Scheduler) Start() {
-	scheduler, err := gocron.NewScheduler()
-	if err != nil {
-		s.log.Error("scheduler init error", "error", err)
-		return
+const (
+	AccessTypeSQL AccessType = "SQL"
+	AccessTypeORM AccessType = "ORM"
+)
+
+func MustLoad() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("no .env file found")
 	}
 
-	_, err = scheduler.NewJob(
-		gocron.DurationJob(seconds*time.Second),
-		gocron.NewTask(s.CheckLinks),
-	)
-	if err != nil {
-		s.log.Error("scheduler job error", "error", err)
-		return
+	botHTTPURL := os.Getenv("BOT_HTTP_URL")
+	if botHTTPURL == "" {
+		botHTTPURL = "http://localhost:8080"
 	}
 
-	s.log.Info("scheduler started", "interval", "30s")
-	scheduler.Start()
-}
-
-func (s *Scheduler) CheckLinks() {
-	ctx := context.Background()
-
-	s.log.Info("checking links")
-
-	links, err := s.repo.GetAllTrackedLinks(ctx)
-	if err != nil {
-		s.log.Error("failed to get tracked links", "error", err)
-		return
+	botGRPCAddr := os.Getenv("BOT_GRPC_ADDR")
+	if botGRPCAddr == "" {
+		botGRPCAddr = "localhost:8082"
 	}
 
-	s.log.Info("links found", "count", len(links))
-
-	for _, link := range links {
-		if err := s.processLink(ctx, link); err != nil {
-			s.log.Warn("link check failed", "url", link.URL, "error", err)
-		}
-	}
-}
-
-func (s *Scheduler) processLink(ctx context.Context, link domain.Link) error {
-	parsed, err := parsers.ParseLink(link.URL)
-	if err != nil {
-		return err
+	scrapperHTTPAddr := os.Getenv("SCRAPPER_HTTP_ADDR")
+	if scrapperHTTPAddr == "" {
+		scrapperHTTPAddr = ":8081"
 	}
 
-	var newUpdatedAt time.Time
-
-	switch parsed.Source {
-	case "github":
-		newUpdatedAt, err = s.githubClient.GetRepoUpdatedAt(
-			ctx,
-			parsed.GithubOwner,
-			parsed.GithubRepo,
-		)
-
-	case "stackoverflow":
-		newUpdatedAt, err = s.soClient.GetQuestionUpdatedAt(
-			ctx,
-			parsed.StackOverflowQuestionID,
-		)
-
-	default:
-		return nil
+	scrapperGRPCAddr := os.Getenv("SCRAPPER_GRPC_ADDR")
+	if scrapperGRPCAddr == "" {
+		scrapperGRPCAddr = ":8083"
 	}
 
-	if err != nil {
-		return err
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		databaseURL = "postgres://postgres:12345@localhost:5432/notesdb?sslmode=disable"
 	}
 
-	if link.LastUpdatedAt.IsZero() {
-		return s.repo.UpdateLastUpdated(ctx, link.URL, newUpdatedAt)
+	accessTypeStr := os.Getenv("ACCESS_TYPE")
+	if accessTypeStr == "" {
+		accessTypeStr = "SQL"
 	}
 
-	if !newUpdatedAt.After(link.LastUpdatedAt) {
-		return nil
+	accessType := AccessType(accessTypeStr)
+	if accessType != AccessTypeSQL && accessType != AccessTypeORM {
+		log.Fatalf("invalid ACCESS_TYPE: %s", accessTypeStr)
 	}
 
-	if err = s.repo.UpdateLastUpdated(ctx, link.URL, newUpdatedAt); err != nil {
-		return err
+	return &Config{
+		BotHTTPURL:       botHTTPURL,
+		BotGRPCAddr:      botGRPCAddr,
+		ScrapperHTTPAddr: scrapperHTTPAddr,
+		ScrapperGRPCAddr: scrapperGRPCAddr,
+		DatabaseURL:      databaseURL,
+		AccessType:       AccessType(accessType),
 	}
-
-	subscribers, err := s.repo.FindSubscribers(ctx, link.URL)
-	if err != nil {
-		return err
-	}
-
-	if len(subscribers) == 0 {
-		return nil
-	}
-
-	update := domain.LinkUpdate{
-		URL:       link.URL,
-		TgChatIDs: subscribers,
-	}
-
-	return s.botClient.SendUpdate(ctx, update)
 }
